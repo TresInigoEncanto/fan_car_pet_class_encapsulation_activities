fc3168a7b0388b2473a383fc7d97bf61c67150bd
Tres <encantotresinigo@gmail.com>
2026-06-01 23:51:17 +0800

    Added the __init__ method with speed, on, radius, and color as its instance variables

--- encapsulation_activities_fan_pet_car_classes/fan_class_program/fan_class_program_functionality.py
+++ encapsulation_activities_fan_pet_car_classes/fan_class_program/fan_class_program_functionality.py
@@ -2,3 +2,9 @@ class Fan:
     SLOW = 1
     MEDIUM = 2
     FAST = 3
+
+    def __init__(self, speed = SLOW, on = False, radius = 5.0, color = 'blue'):
+        self.__speed = speed
+        self.__on = on
+        self.__radius = float(radius)
+        self.__color = color
