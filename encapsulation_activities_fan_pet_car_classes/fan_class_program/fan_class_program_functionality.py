ef003d2a931605f0311f33386654f06baf9b2cc4
Tres <encantotresinigo@gmail.com>
2026-06-02 00:28:24 +0800

    Added the setters and the getters of each instance variable data fields

--- encapsulation_activities_fan_pet_car_classes/fan_class_program/fan_class_program_functionality.py
+++ encapsulation_activities_fan_pet_car_classes/fan_class_program/fan_class_program_functionality.py
@@ -8,3 +8,28 @@ class Fan:
         self.__on = on
         self.__radius = float(radius)
         self.__color = color
+
+    def set_speed(self, speed):
+        if speed in [Fan.SLOW, Fan.MEDIUM, Fan.FAST]:
+            self.__speed = speed
+    
+    def get_speed(self):
+        return self.__speed
+    
+    def set_on(self, on):
+        self.__on = on
+    
+    def get_on(self):
+        return self.__on
+    
+    def set_radius(self, radius):
+        self.__radius = float(radius)
+    
+    def get_radius(self):
+        return self.__radius
+    
+    def set_color(self, color):
+        self.__color = color
+    
+    def get_color(self):
+        return self.__color
