1bad954841ba51a7b4f83085502dbbcc8b79b11f
Tres <encantotresinigo@gmail.com>
2026-06-02 00:54:44 +0800

    Added the two objects (fan 1 and fan 2) together with their desired values as well as the default values of the parametarized constructors.

--- encapsulation_activities_fan_pet_car_classes/fan_class_program/TestFan.py
+++ encapsulation_activities_fan_pet_car_classes/fan_class_program/TestFan.py
@@ -1,3 +1,5 @@
 from fan_class_program_functionality import Fan
 
 def TestFan():
+    fan1 = Fan(speed=Fan.FAST, radius = 10, color = 'yellow', on = True)
+    fan2 = Fan(speed=Fan.MEDIUM, radius = 5, color = 'blue', on = False)
