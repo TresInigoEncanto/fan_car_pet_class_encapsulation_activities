dd75bf1eb40b84217bdf2c04e21c997eea5dc64d
Tres <encantotresinigo@gmail.com>
2026-06-04 02:56:15 +0800

    Created a for loop with 5 as the argument to be able to call the accelerate class method 5 times, used an index to print every call with the current speed until it reaches 5

--- encapsulation_activities_fan_pet_car_classes/car_class_program/car_program_main.py
+++ encapsulation_activities_fan_pet_car_classes/car_class_program/car_program_main.py
@@ -2,3 +2,12 @@ from car_class_program_functionality import Car
 
 def main():
     my_car = Car(2026, "Toyota Corolla")
+
+    print("--- Acceleration ---")
+
+    for i in range(5):
+        my_car.accelerate()
+        print(f"Current speed after call {i+1}: {my_car.get_speed()}")
+
+if __name__ == "__main__":
+    main()
