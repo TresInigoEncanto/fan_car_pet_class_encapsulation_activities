0097eb2da718acd6d258946af309313b1e3cffde
Tres <encantotresinigo@gmail.com>
2026-06-04 03:02:29 +0800

    Created another for loop with 5 as an argument to call the brake method 5 times. Created an index to be able to print the current speed every time the program calls the brake method

--- encapsulation_activities_fan_pet_car_classes/car_class_program/car_program_main.py
+++ encapsulation_activities_fan_pet_car_classes/car_class_program/car_program_main.py
@@ -7,7 +7,12 @@ def main():
 
     for i in range(5):
         my_car.accelerate()
-        print(f"Current speed after call {i+1}: {my_car.get_speed()}")
+        print(f"Current speed after accelerate call {i+1}: {my_car.get_speed()}")
+    
+    print("\n--- Brake ---")
+    for i in range(5):
+        my_car.brake()
+        print(f"Current speed after brake call {i+1}: {my_car.get_speed()}")
 
 if __name__ == "__main__":
     main()
