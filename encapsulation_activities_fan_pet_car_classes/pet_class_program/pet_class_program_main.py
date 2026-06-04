ecb9502f9879fe3468fc618ac6c9a7a4a6afa6c4
Tres <encantotresinigo@gmail.com>
2026-06-04 14:45:54 +0800

    Created an object of the pet class and named it my_pet, added input_name, input_animal_type, and input_age as its attributes

--- encapsulation_activities_fan_pet_car_classes/pet_class_program/pet_class_program_main.py
+++ encapsulation_activities_fan_pet_car_classes/pet_class_program/pet_class_program_main.py
@@ -1,1 +1,8 @@
 from pet_class_program_functionality import Pet
+
+def main():
+    my_pet = Pet(input_name, input_animal_type, input_age)
+
+    input_name = 
+    input_animal_type =
+    input_age =
