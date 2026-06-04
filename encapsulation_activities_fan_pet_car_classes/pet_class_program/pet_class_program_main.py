4e0f3c64e56e23c479cd8357483a123eb652e3e2
Tres <encantotresinigo@gmail.com>
2026-06-04 15:08:04 +0800

    Added the last accessor 'getter' methods for the animal type and age to be able to display it after the user inputs the information needed

--- encapsulation_activities_fan_pet_car_classes/pet_class_program/pet_class_program_main.py
+++ encapsulation_activities_fan_pet_car_classes/pet_class_program/pet_class_program_main.py
@@ -3,12 +3,14 @@ from pet_class_program_functionality import Pet
 def main():
     input_name = input("Enter your pet's name: ")
     input_animal_type = input("Enter your pet's animal type: ")
-    input_age = input("Enter your pet's age: ")
+    input_age = input("Enter your pet's age (Ex: 1 year): ")
 
     my_pet = Pet(input_name, input_animal_type, input_age)
 
     print("\n--- Your Pet's Information ---")
     print("Your pet's name is", my_pet.get_name())
+    print("Your pet is a", my_pet.get_animal_type())
+    print("Your pet is", my_pet.get_age(), "old")
 
 if __name__ == "__main__":
     main()
